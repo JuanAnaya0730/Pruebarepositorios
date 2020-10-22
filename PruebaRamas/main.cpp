6fc530c0521a5f59aa474efd160c6f1bc04c64e3
Juan Anaya <juan.anayar@udea.edu.co>
2020-10-22 08:58:01 -0500

    variable que almacena la cadena y la imprime caracter a caracter.

--- PruebaRamas/main.cpp
+++ PruebaRamas/main.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+// Variables y macros
+#define TAM (int)10
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    char a[TAM]= "AN11CD44";
+
+    for(int var=0; var<TAM; ++var){
+        cout << a[var] << endl;
+    }
+
     return 0;
 }
