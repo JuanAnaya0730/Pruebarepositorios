574ce39cd8692193c60366204ad3f8bcfd614f39
Juan Anaya <juan.anayar@udea.edu.co>
2020-10-22 09:18:41 -0500

    Impresión de los números de la cadena original.

--- PruebaRamas/main.cpp
+++ PruebaRamas/main.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 
 // Variables y macros
-#define TAM (int)10
+#define TAM (int)50
 
 int main()
 {
-    char a[TAM]= "AN11CD44";
+    char a[TAM]= "AN11CD44EFF5X5";
 
     for(int var=0; var<TAM; ++var){
-        cout << a[var] << endl;
+        if(a[var]!='\0'){
+            cout << a[var] << endl;
+        }
     }
 
     cout << "Imprimir los numeros de la cadena original" << endl;
     for(int var=0; var<TAM; ++var){
-        if(a[var]>='0' or a[var]<='9'){
+        if(a[var]>='0' && a[var]<='9'){
             cout << a[var] << endl;
         }
     }
