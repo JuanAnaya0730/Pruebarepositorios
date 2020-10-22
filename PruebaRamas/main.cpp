dbf70bc493bca91a00df31ed1ca9a36a63c18084
Juan Anaya <juan.anayar@udea.edu.co>
2020-10-22 09:09:00 -0500

    3do commit

--- PruebaRamas/main.cpp
+++ PruebaRamas/main.cpp
@@ -13,5 +13,12 @@ int main()
         cout << a[var] << endl;
     }
 
+    cout << "Imprimir los numeros de la cadena original" << endl;
+    for(int var=0; var<TAM; ++var){
+        if(a[var]>='0' or a[var]<='9'){
+            cout << a[var] << endl;
+        }
+    }
+
     return 0;
 }
